4eefb0e282e0612f71a8395d9f38a6d63c19f99b
mohamed salah <mohamedsalahsabry88@gmail.com>
2025-02-05 17:21:25 +0200

    Build Contact Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutUsComponent } from "./pages/about-us/about-us.component";
+import { ContactUsComponent } from "./pages/contact-us/contact-us.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, AboutUsComponent],
+  imports: [RouterOutlet, HomeComponent, AboutUsComponent, ContactUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
