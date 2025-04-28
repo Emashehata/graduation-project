afc2a8ff08f1c6c5b48597f04015a5491c19390a
mohamed salah <mohamedsalahsabry88@gmail.com>
2025-04-29 01:45:32 +0300

    CreateFeedbackInAdmin

--- src/app/core/services/feedback/feedback.service.ts
+++ src/app/core/services/feedback/feedback.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { Feedback } from '../../interfaces/IFeedback/ifeedback';
 
 @Injectable({
   providedIn: 'root'
@@ -17,10 +18,12 @@ export class FeedbackService {
     return this.httpClient.post(`${environment.baseUrl}api/Feedback` , data)
   }
 
-  getFeedback():Observable<any>
-  {
-    return this.httpClient.get(`${environment.baseUrl}api/Feedback`)
+  // getFeedback():Observable<any>
+  // {
+  //   return this.httpClient.get(`${environment.baseUrl}api/Feedback`)
+  // }
+
+  getFeedback(): Observable<Feedback[]> {
+    return this.httpClient.get<Feedback[]>(`${environment.baseUrl}api/Feedback`);
   }
-
-
 }
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,26 @@ import { AppComponent } from './app/app.component';
 import {register as registerSwiper} from 'swiper/element-bundle';
 import { registerLocaleData } from '@angular/common';
 import localeAr from '@angular/common/locales/ar';
+import { provideCharts } from 'ng2-charts';
+
+// registerSwiper();
+
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+
+  
+// registerLocaleData(localeAr);
+
 
 registerSwiper();
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    provideCharts(), // 🔥 توفير charts مع البروڤايدرز
+    ...(appConfig.providers || [])
+  ]
+})
   .catch((err) => console.error(err));
 
-  
 registerLocaleData(localeAr);
