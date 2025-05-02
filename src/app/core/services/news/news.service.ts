f385efda886ead08719278186389af890f5c00a3
Eman_Shehata <emanshehata258@gmail.com>
2025-05-02 21:03:25 +0300

    convert the api for news and add new api update news

--- src/app/core/services/news/news.service.ts
+++ src/app/core/services/news/news.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,24 +11,27 @@ export class NewsService {
   constructor( private httpClient:HttpClient ) { }
 
   getAllNews():Observable<any>{
-    return this.httpClient.get('http://medicalcaree.runasp.net/api/NewsPost');
+    return this.httpClient.get(`${environment.baseUrl}api/NewsPost`);
   }
 
 
 
   getSpecficNews(id:string | null):Observable<any>{
-    return this.httpClient.get(`http://medicalcaree.runasp.net/api/NewsPost/${id}`);
+    return this.httpClient.get(`${environment.baseUrl}api/NewsPost/${id}`);
   }
 
 
 
   postNews(data:object):Observable<any>{
-    return this.httpClient.post('http://medicalcaree.runasp.net/api/NewsPost',data);
+    return this.httpClient.post(`${environment.baseUrl}api/NewsPost`,data);
+  }
+  updateNews(data:object,id:string | null):Observable<any>{
+    return this.httpClient.put(`${environment.baseUrl}api/NewsPost/${id}`,data);
   }
 
 
   deleteSpecficNews(id:string | null):Observable<any>{
-    return this.httpClient.delete(`http://medicalcaree.runasp.net/api/NewsPost/${id}`);
+    return this.httpClient.delete(`${environment.baseUrl}api/NewsPost/${id}`);
   }
 
 
